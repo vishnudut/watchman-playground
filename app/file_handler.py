9be93aed2b6a3a55675f81451564b8d2dd34e2ed
Vishnu Dut <vvdut99@gmail.com>
2025-10-10 18:19:16 -0400

    security: fix unsafe-deserialization in app/file_handler.py

--- app/file_handler.py
+++ app/file_handler.py
@@ -10,11 +10,7 @@ def read_user_file(filename):
     with open(file_path, 'r') as f:
         return f.read()
 
-def deserialize_data(data):
-    # VULNERABLE: Arbitrary code execution via pickle
-    return pickle.loads(base64.b64decode(data))
-
-def download_file():
+json.loads(base64.b64decode(data).decode('utf-8'))
     filename = request.args.get('file')
 
     # VULNERABLE: Directory traversal
